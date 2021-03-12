6e8e44dbe9eb564199e3fe3549e9b20e91d024d3
Lucas Joviniano <lucasjoviniano1@gmail.com>
2021-03-12 14:43:42 -0300

    Basic Folder Structure

--- app/src/main/java/com/oslu/forall/views/MainActivity.kt
+++ app/src/main/java/com/oslu/forall/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.oslu.forall
+package com.oslu.forall.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.oslu.forall.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
